84c1caba3ab2052360de4e8209a56b871cd526a3
DmytroHladkyi <dmytrogladkyi8@gamil.com>
2023-04-29 11:02:33 +0200

    Upload files to Google Drive

---
+++ main.py
@@ -0,0 +1,61 @@
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
+import os
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+creds = None
+
+if os.path.exists ("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token: creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file("client_secrets2.0.json", SCOPES)
+        creds = flow.run_local_server(port=0)
+
+    with open('token,json', 'w') as token:
+        token.write(creds.to_json())
+
+try:
+    service = build("drive", "v3", credentials=creds)
+    response = service.files().list(
+    q="name='Honda' and mimeType='application/vnd.google-apps.folder'",
+    spaces='drive'
+    ).execute()
+
+    if not response['files']:
+        file_metadata = {
+            "name": "Honda",
+            "mimeType": "application/vnd.google-apps.folder"
+        }
+
+        file = service.files().create(body=file_metadata, fields="id").execute()
+
+        folder_id = file.get('id')
+    else:
+        folder_id = response['files'][0]['id']
+
+    for file in os.listdir(r'C:\Users\Hladkyi Dmytro\Desktop\IT Oprogromowanie\Python_Projects\project6_Saving_files_to_Google_cloud\honda'):
+        file_metadata = {
+            "name": file,
+            "parents": [folder_id]
+        }
+
+        media = MediaFileUpload(f"honda/{file}")
+        upload_file = service.files().create(body=file_metadata, 
+                                             media_body=media, 
+                                             fields="id").execute()
+        print("Backed up file: " + file)
+
+except HttpError as e:
+    print("Error: " + str(e))
